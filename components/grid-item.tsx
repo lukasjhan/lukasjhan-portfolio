394b658be47465cf6a74ae528cd261ae845a27af
Lukas.J.Han <lukas.j.han@gmail.com>
2023-12-30 13:03:39 +0900

    add new grid size

--- components/grid-item.tsx
+++ components/grid-item.tsx
@@ -1,22 +1,23 @@
-"use client";
-import { cn } from "@/utils/cn";
-import { cva, type VariantProps } from "class-variance-authority";
-import { motion } from "framer-motion";
+'use client';
+import { cn } from '@/utils/cn';
+import { cva, type VariantProps } from 'class-variance-authority';
+import { motion } from 'framer-motion';
 
 const variants = cva(
-  "shadow-grid rounded-3xl bg-white dark:bg-neutral-900 flex flex-col justify-center @container",
+  'shadow-grid rounded-3xl bg-white dark:bg-neutral-900 flex flex-col justify-center @container',
   {
     variants: {
       size: {
-        "1x2": "md:col-span-1 col-span-2 row-span-2 md:p-8 p-4",
-        "2x1": "md:col-span-2 col-span-full row-span-1 py-4 md:px-8 px-4",
-        "2x2": "md:col-span-2 col-span-full row-span-2 md:p-8 p-4",
-        "2x4":
-          "md:col-span-2 col-span-full row-span-4 relative overflow-hidden",
+        '1x2': 'md:col-span-1 col-span-2 row-span-2 md:p-8 p-4',
+        '1x4': 'md:col-span-1 col-span-2 row-span-4 md:p-8 p-4',
+        '2x1': 'md:col-span-2 col-span-full row-span-1 py-4 md:px-8 px-4',
+        '2x2': 'md:col-span-2 col-span-full row-span-2 md:p-8 p-4',
+        '2x4':
+          'md:col-span-2 col-span-full row-span-4 relative overflow-hidden',
       },
     },
     defaultVariants: {
-      size: "1x2",
+      size: '1x2',
     },
   }
 );
@@ -37,7 +38,7 @@ const GridItem = ({ size, children }: GridItemProps) => {
         variants({
           size,
           className:
-            "hover:dark:bg-neutral-800 hover:bg-neutral-50 duration-75 transition-colors ease-in-out",
+            'hover:dark:bg-neutral-800 hover:bg-neutral-50 duration-75 transition-colors ease-in-out',
         })
       )}
     >
