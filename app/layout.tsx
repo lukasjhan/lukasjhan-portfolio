4acd7237b63e4c42313aa4ffc09549e942925e0b
Lukas.J.Han <lukas.j.han@gmail.com>
2023-12-30 12:00:12 +0900

    feat: change title and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,46 +1,46 @@
-import { ThemeProvider } from "@/components/theme-provider";
-import localFont from "next/font/local";
-import "./globals.css";
+import { ThemeProvider } from '@/components/theme-provider';
+import localFont from 'next/font/local';
+import './globals.css';
 
 const sfPRO = localFont({
   src: [
     {
-      path: "./fonts/sf-pro-display_thin.woff2",
-      weight: "100",
-      style: "normal",
+      path: './fonts/sf-pro-display_thin.woff2',
+      weight: '100',
+      style: 'normal',
     },
     {
-      path: "./fonts/sf-pro-display_light.woff2",
-      weight: "300",
-      style: "normal",
+      path: './fonts/sf-pro-display_light.woff2',
+      weight: '300',
+      style: 'normal',
     },
     {
-      path: "./fonts/sf-pro-display_regular.woff2",
-      weight: "400",
-      style: "normal",
+      path: './fonts/sf-pro-display_regular.woff2',
+      weight: '400',
+      style: 'normal',
     },
     {
-      path: "./fonts/sf-pro-display_medium.woff2",
-      weight: "500",
-      style: "normal",
+      path: './fonts/sf-pro-display_medium.woff2',
+      weight: '500',
+      style: 'normal',
     },
     {
-      path: "./fonts/sf-pro-display_semibold.woff2",
-      weight: "600",
-      style: "normal",
+      path: './fonts/sf-pro-display_semibold.woff2',
+      weight: '600',
+      style: 'normal',
     },
     {
-      path: "./fonts/sf-pro-display_bold.woff2",
-      weight: "700",
-      style: "normal",
+      path: './fonts/sf-pro-display_bold.woff2',
+      weight: '700',
+      style: 'normal',
     },
   ],
-  variable: "--font-sfpro",
+  variable: '--font-sfpro',
 });
 
 export const metadata = {
-  title: "makrdev",
-  description: "A create video tutorials for Next.js, Supabase and OpenAI.",
+  title: 'Lukas J Han',
+  description: 'Backends Developer & Cloud Architect',
 };
 
 export default function RootLayout({
