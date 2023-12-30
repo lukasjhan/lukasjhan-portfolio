e4e9796d8472ee5fdec5e29224b05801f13ff01c
Lukas.J.Han <lukas.j.han@gmail.com>
2023-12-30 13:03:55 +0900

    feat: add gradient background

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import Footer from "@/components/footer";
-import LeftPanel from "@/components/left-panel";
-import RightPanel from "@/components/right-panel";
+import Footer from '@/components/footer';
+import LeftPanel from '@/components/left-panel';
+import RightPanel from '@/components/right-panel';
 
 export default function Home() {
   return (
@@ -8,7 +8,7 @@ export default function Home() {
       {/* Pattern */}
       <div className="absolute inset-0 z-0 bg-light-pattern dark:bg-dark-pattern bg-verySmall" />
       {/* Overlay */}
-      <div className="absolute inset-0 z-10 bg-gradient-to-b from-white via-white/90 to-white dark:from-neutral-950 dark:via-neutral-950/90 dark:to-neutral-950" />
+      <div className="absolute inset-0 z-10 bg-gradient-to-b from-white via-white/90 to-[#9cd9eb] dark:from-neutral-950 dark:via-neutral-950/90 dark:to-[#1e2d31]" />
       <div className="container relative z-20 flex flex-col w-full h-full px-6 gap:6 xl:gap-10 xl:flex-row">
         {/* Left Panel */}
         <LeftPanel />
