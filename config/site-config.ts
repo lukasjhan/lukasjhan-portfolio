69e498b92683be11984d34ba33e9e09d4cfbb770
Lukas.J.Han <lukas.j.han@gmail.com>
2023-12-30 11:40:24 +0900

    feat: change the name

--- config/site-config.ts
+++ config/site-config.ts
@@ -166,12 +166,11 @@ const GridItems: GridItemInterface[] = [
 ];
 
 export const siteConfig = {
-  creator: 'Batuhan Bilgin',
-  title: 'Developer & Creator',
-  bio: 'Building AI products and creating video tutorials on Youtube.',
-  location: 'Istanbul, Turkey',
-  locationLink:
-    'https://www.google.com/maps/place/Kad%C4%B1k%C3%B6y%2F%C4%B0stanbul/@40.981133,29.063053,12z/data=!3m1!4b1!4m6!3m5!1s0x14cac790b17ba89d:0xd2d24ea0437a7ee2!8m2!3d40.981867!4d29.0576302!16zL20vMDV5cWY4?entry=ttu',
-  email: 'batuhanbilginn@gmail.com',
+  creator: 'Lukas J Han',
+  title: 'Backends Developer & Cloud Architect',
+  bio: 'Building scalable and reliable distributed systems.',
+  location: 'Palo Alto / US',
+  locationLink: 'https://maps.app.goo.gl/tZAcoetsuALLDfzQ7',
+  email: 'lukas.j.han@gmail.com',
   items: GridItems,
 } as const;
